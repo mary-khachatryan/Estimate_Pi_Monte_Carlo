9a69939e31e26a1985f0ee273ccce05cd54e91fa
mary-ukee <mary.khachatryan2@gmail.com>
2022-10-16 17:55:41 +0400

    adds all the files

---
+++ Assets — копия/scripts/ButtonCode.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonCode : MonoBehaviour
+{
+    
+    public void ResScene()
+    {
+        SceneManager.LoadScene("ResultScene");
+
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
